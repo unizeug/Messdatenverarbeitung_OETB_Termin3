9e92829e7ac4b80c5705e4b815c3cd6ba59413de
Boris Henckell <borishenckell@gmail.com>
2012-05-23 15:29:36 +0200

    alphadetektor

--- PR MDV - Termin 3/Matlab/Alpha Hauptprogramm.m
+++ PR MDV - Termin 3/Matlab/Alpha Hauptprogramm.m
@@ -1,3 +1,22 @@
 %alphahauptprogramm
-load Anschnitt_pi_2;
-alphadetektor(anschnitt_pi_2, 50, 6000, 176)
+%load Anschnitt_pi_2;
+%figure(1)
+%plot(alpha0);
+
+%figure(2)
+%plot(abs(alpha0));
+
+figure(3)
+plot(steigung)
+
+figure(4)
+plot(abs(steigung));
+
+
+
+
+
+
+
+
+%alphadetektor(anschnitt_pi_2, 50, 6000, 176)
--- PR MDV - Termin 3/Matlab/alphadetektor.m
+++ PR MDV - Termin 3/Matlab/alphadetektor.m
@@ -1,4 +1,4 @@
-function [alpha,check]=alphadetektor(uc, f, samplerate, numofsample)
+function [alpha,check,steigung]=alphadetektor(uc, f, samplerate, numofsample)
 %function i = stromPhasSchnitt(id,f,alpha,t)
 %---------------------------------------------------
 % filename:         stromPhasSchnitt
@@ -33,34 +33,56 @@ mittel = mean(it);
 
 %Schleife über alle Abtastwerte
 %zählvariable der Schleife
-k=1;
+k=10;
 
 warschonmalnichtnull = 0;
 relativwertvonk = 1;
 alpha = -1;
 check = [-1,-1,-1,-1,-1];
+steigung = zeros(175,1);
 
+
+% while  k<(length(t)-1)
+%     if (abs(it(k))>(mittel+0.03)&&(relativwertvonk==1))
+%         k=k+1;
+%         warschonmalnichtnull = 1;
+%         check(1) = k;
+%     elseif ((abs(it(k)))<(mittel+0.03) && (warschonmalnichtnull == 0))
+%             k=k+1;
+%             check(2) = k;
+%     elseif ((abs(it(k)))<(mittel+0.03) && (warschonmalnichtnull==1))
+%         k=k+1;
+%         relativwertvonk = relativwertvonk +1;
+%         check(3)=k;
+%     else
+%         alpha = relativwertvonk/((T_periode/2)*samplerate);
+%         check(4) = k;
+%         check(5) = relativwertvonk;
+%         k=length(t)+1;
+%     end;
+%        
+% end;
 
 while  k<(length(t)-1)
-    if (abs(it(k))>(mittel+0.03)&&(relativwertvonk==1))
+    if ((abs(it(k)-it(k-9))>(-0.004))&&(relativwertvonk<2))
         k=k+1;
         warschonmalnichtnull = 1;
         check(1) = k;
-    elseif ((abs(it(k)))<(mittel+0.03) && (warschonmalnichtnull == 0))
+        steigung(k) = it(k)-it(k-9);
+    elseif ((abs(it(k)-it(k-1))<(0.004)) && (warschonmalnichtnull == 0))
             k=k+1;
             check(2) = k;
-    elseif ((abs(it(k)))<(mittel+0.03) && (warschonmalnichtnull==1))
+    elseif ((abs(it(k)-it(k-1))<(0.004)) && (warschonmalnichtnull==1))
         k=k+1;
         relativwertvonk = relativwertvonk +1;
         check(3)=k;
     else
-        alpha = relativwertvonk/((T_periode/2)*samplerate);
+        alpha = (relativwertvonk-1)/((T_periode/2)*samplerate);
         check(4) = k;
         check(5) = relativwertvonk;
         k=length(t)+1;
     end;
        
 end;
-
 
 
